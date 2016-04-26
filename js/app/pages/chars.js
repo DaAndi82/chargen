4e611522a3c92dfce6420b50eef80f42add9b138
Schiefele.Andreas <Da.Andi@web.de>
2016-04-26 23:50:43 +0200

    - New styling for talents

--- js/app/pages/chars.js
+++ js/app/pages/chars.js
@@ -25,6 +25,25 @@
 		};
 	})
 	
+	.filter('orderObjectBy', function () {
+		return function (input, attribute) {
+			if (!angular.isObject(input)) return input;
+
+			var array = [];
+			for(var objectKey in input) {
+				array.push(input[objectKey]);
+			}
+
+			array.sort(function(a, b){
+				if(a[attribute] < b[attribute]) return -1;
+				if(a[attribute] > b[attribute]) return 1;
+				return 0;
+			});
+			
+			return array;
+		}
+	})
+	
 	.directive('popover', function() {
 		return function(scope, elem, attrs) {
 			elem;
